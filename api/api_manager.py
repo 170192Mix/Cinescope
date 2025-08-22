08e300d32aa9409722563a541091442a7cb4b085
Maxim <maxim.antonov92@gmail.com>
2025-08-23 00:54:17 +0300

    Fix: объединён ApiManager, добавлен auth_api

--- api/api_manager.py
+++ api/api_manager.py
@@ -1,21 +1,20 @@
+
+import requests
+
 from constants import API_BASE, HEADERS
 from api.movies_api import MoviesAPI
-from .auth_api import AuthAPI
-from .user_api import UserAPI
+from api.auth_api import AuthAPI
+from api.user_api import UserAPI  # если есть такой класс
 
 class ApiManager:
     """
-    Держит инстансы API-классов на одной сессии.
+    Единая точка доступа к API-клиентам, все работают на одной requests.Session()
     """
-    def __init__(self, session):
-        self.session = session
-        self.auth_api = AuthAPI(session)
-        self.user_api = UserAPI(session)
+    def __init__(self, session: requests.Session | None = None):
+        self.session = session or requests.Session()
 
-class ApiManager:
-    def __init__(self, session):
-        self.session = session
-        # уже было: self.auth_api = AuthAPI(...)
+        self.auth_api = AuthAPI(self.session)
+        self.user_api = UserAPI(self.session) if 'UserAPI' in globals() else None
 
     @property
     def movies_api(self) -> MoviesAPI:
